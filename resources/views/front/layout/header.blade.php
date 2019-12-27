c0258006ab838540939ebd3dc06adba836651eed
TheSoloGod <kienaof94@gmail.com>
2019-12-27 23:30:46 +0700

    edit form register

--- resources/views/front/layout/header.blade.php
+++ resources/views/front/layout/header.blade.php
@@ -105,13 +105,17 @@
                             </li>
                         @endif
                     @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle active" href="#" role="button"
+                        <li class="nav-item dropdown active">
+                            <a id="navbarDropdown" class="nav-link nav-link-grow-up dropdown-toggle" href="#" role="button"
                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <div class="dropdown-menu bg-orange mt-4" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">
+                                    Hồ sơ
+                                </a>
+                                <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
