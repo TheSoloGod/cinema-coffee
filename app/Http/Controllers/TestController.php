efb18010f2997ac1a0ac6e9d65a51555f7294a96
KienPT <kienaof94@gmail.com>
2020-03-08 03:23:43 +0700

    test get post request

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 class TestController extends Controller
 {
     public function test(Request $request) {
-        dd($request);
+        $user = $request->user;
+        $token = $request->token;
+        $data = [
+            'user' => $user,
+            'token' => $token
+        ];
+        return response()->json($data, 200);
     }
 }
