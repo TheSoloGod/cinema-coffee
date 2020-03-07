8d176e8ee654a31967bc0d9113104b547cc37170
KienPT <kienaof94@gmail.com>
2020-03-08 03:42:20 +0700

    test api

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/test', 'TestController@test')->name('test');
+Route::get('/test', 'TestController@test')->name('test');
